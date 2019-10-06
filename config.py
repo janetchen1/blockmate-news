f6c58673f6e1fb80106cc5d6aaf6be46aa015eac
janetchen1 <janetchen@college.harvard.edu>
2019-10-06 01:35:19 -0400

    config env

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+SENDER = os.getenv('SENDER', None)
+TEST_RECIPIENT = os.getenv('TEST_RECIPIENT', None)
+
+GROUPS_SPREADSHEET = os.getenv('GROUPS_SPREADSHEET', None)
+USERS_SPREADSHEET = os.getenv('USERS_SPREADSHEET', None)
